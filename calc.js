c9f8f3dad8cfec88607b976059825bacd64306ac
seth <seth@dcdesigns.io>
2017-11-23 09:57:23 -0600

    added in equals, need to figure out how to clear after

--- calc.js
+++ calc.js
@@ -4,23 +4,46 @@ Created: 11/22/17
 Version: 0.0.1
 
 	*Table of Contents*
-		1. Number Key
+		0. Variables and Functions
+		1. Select Number
 		2. Clear Button
 		3. Addition
+		4. Subtraction
+		5. Multiplication
+		6. Division
 *********************************************************/
-// Placeholder
+
+
+/* 0. Variables and Functions
+*************************/
 let number = '';
 let total = 0;
 let stageArr = [];
-let check = 0;
+let check = false;
+let after = false;
+let eh = '';
 
-/* 1. Create a number
+function stageNumber() {
+	stageArr.push(number);
+	number = '';
+
+	if ( stageArr.length > 1 ) {
+		stageArr.shift();
+		check = true;
+	}
+}
+
+
+
+
+/* 1. Select Number
 *************************/
 $('.numKey').click( function() {	
 	let numVal = $(this).val();	
-	number += numVal;	
+	number += numVal;
 	$('#display').val(number);
 });
+
 
 /* 2. Clear Button
 *************************/
@@ -28,41 +51,32 @@ $('#clearButton').click( function() {
 	number = '';
 	stageArr = [];
 	total = 0;
-	check = 0;
+	check = false;
+	after = false;
 	$('#display').val('');
-
 });
+
 
 /* 3. Addition
 *************************/
 $('#addButton').on('click', addition);
 
 function addition() {
-	stageArr.push(number);
-	number = '';
-
-	if ( stageArr.length > 1 ) {
-		stageArr.shift();
-	}
-	
+	stageNumber();	
+	eh = '+';
 	total += Number(stageArr[0]);
 	$('#display').val(total);
 }
+
 
 /* 4. Subtraction
 *************************/
 $('#subtractButton').on('click', subtraction);
 
 function subtraction() {
-	stageArr.push(number);
-	number = '';
-
-	if ( stageArr.length > 1 ) {
-		stageArr.shift();
-		check = 1;
-	}
-
-	if ( check === 0 ) {
+	stageNumber();
+	eh = '-';
+	if ( !check ) {
 		total = Number(stageArr[0]);
 	}
 	else {
@@ -71,20 +85,15 @@ function subtraction() {
 	}	
 }
 
+
 /* 5. Multiplication
 *************************/
 $('#multiplyButton').on('click', multiply);
 
 function multiply() {
-	stageArr.push(number);
-	number = '';
-
-	if ( stageArr.length > 1 ) {
-		stageArr.shift();
-		check = 1;
-	}
-
-	if ( check === 0 ) {
+	stageNumber();	
+	eh = '*';
+	if ( !check ) {
 		total = Number(stageArr[0]); 
 	}
 	else {
@@ -96,3 +105,42 @@ function multiply() {
 
 /* 6. Division
 *************************/
+$('#divideButton').on('click', division);
+
+function division() {
+	stageNumber();
+	eh = '/';
+	if ( !check ) {
+		total = Number(stageArr[0]);
+	}
+	else {
+		total /= Number(stageArr[0]);
+		$('#display').val(total);
+	}
+}
+
+
+/*7 Equation
+*************************/
+$('#equalsButton').on('click', equation);
+
+function equation() {
+	after = true;
+	let equations = {
+		'+': addition,
+		'-': subtraction,
+		'*': multiply,
+		'/': division
+	};
+	return equations[eh]();
+}
+
+while ( after ) {
+	$('#equalsButton').on('click', silly);
+
+	function silly() {
+		console.log('you are a silly bitch!!');
+		after = false;
+	}
+
+}
